3d4334af35f58aceebf416f15e44ef27a2b20571
José Pereira Macedo <arseniumgx@gmail.com>
2021-07-24 00:03:45 -0300

    arquivo de configuraçao do flask_mail com as informaçoes do email de test criado

---
+++ semana2/mail_settings.py
@@ -0,0 +1,8 @@
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com', 
+    "MAIL_PORT": 465, 
+    "MAIL_USE_TLS": False, 
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": 'c003.teste.jp@gmail.com',
+    "MAIL_PASSWORD": 'C003.teste'
+}
